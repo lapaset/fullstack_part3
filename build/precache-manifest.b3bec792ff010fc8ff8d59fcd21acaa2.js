2d725afb11aa4e0e50ca5811176919d27869ff58
llahti <llahti@student.hive.fi>
2020-04-16 16:31:16 +0300

    Add frontend build

---
+++ build/precache-manifest.b3bec792ff010fc8ff8d59fcd21acaa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a014138d517466c3075ea14165317c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf34a52b42fb5cde636a",
+    "url": "/static/js/2.5bde2c0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bde2c0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd0f1b347a8b11deb001",
+    "url": "/static/js/main.873f8c09.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);

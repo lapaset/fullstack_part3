cfbce04f69869d7604d53b8f6d3e1451bd0d54c2
llahti <llahti@student.hive.fi>
2020-04-16 16:02:15 +0300

    Make backend ready for deployment in Heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('data', (req, res) => {
     if (req.method == 'POST')
@@ -93,7 +95,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
